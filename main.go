86b0dcb8bf0f08c583ebc3318287132b293e4e47
Boris Lassort <163381+borislassort@users.noreply.github.com>
2024-04-10 14:31:01 +0200

    Refactor

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+// Need CGO_ENABLED=1
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var urlMap = make(map[string]string)
+var rng *rand.Rand
+
+func main() {
+	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+	r := gin.Default()
+	r.POST("/shorten", shortURL)
+	r.GET("/:shortURL", redirectURL)
+	r.Run(":8080")
+}
+
+func shortURL(c *gin.Context) {
+	longURL := c.Param("url")
+	shortURL := genShortURL()
+	urlMap[shortURL] = longURL
+	c.JSON(http.StatusOK, gin.H{"shortURL": shortURL})
+}
+
+func redirectURL(c *gin.Context) {
+	shortURL := c.Param("shortURL")
+	longURL, ok := urlMap[shortURL]
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Short URL not found"})
+		return
+	}
+	c.Redirect(http.StatusMovedPermanently, longURL)
+}
+
+func genShortURL() string {
+	const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	shortURL := make([]byte, 6)
+	for i := range shortURL {
+		shortURL[i] = charset[rng.Intn(len(charset))]
+	}
+	return string(shortURL)
+}
